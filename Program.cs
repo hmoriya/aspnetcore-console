12c6175268049de6d5a544fb7d68eff8be10c1f5
hmoriya <hmoriya@archway.co.jp>
2017-02-27 12:07:46 +0900

    Init Console App

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Logging;
+
+namespace aspnetcore_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            services.AddLogging();
+
+            foreach (var service in services)
+            {
+                System.Console.WriteLine(service.ServiceType.ToString());
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
