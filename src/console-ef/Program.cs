d45671a396eb9f04247db302658a97bcd53afd3e
森屋英治 <hmoriya@hmoriya-mbpro.local>
2017-03-15 20:58:19 +0900

    add console template

---
+++ src/console-ef/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console_ef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
