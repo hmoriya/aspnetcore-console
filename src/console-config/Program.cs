e070eb693dc4b6d3faf72e21d756f49fee6e5598
森屋英治 <hmoriya@hmoriya-mbpro.local>
2017-03-16 23:48:38 +0900

    add configration sample

---
+++ src/console-config/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace console_config
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            System.Console.WriteLine(configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
