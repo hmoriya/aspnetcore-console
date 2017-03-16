893f4ddd5f8e7b2a441d5400ef38fa147b3c226c
森屋英治 <hmoriya@hmoriya-mbpro.local>
2017-03-16 22:55:23 +0900

    add logger sample

---
+++ src/console-log/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace console_log
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging();
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var factory = serviceProvider.GetService<ILoggerFactory>();
+            factory.AddConsole();
+
+            var logger = factory.CreateLogger<Program>();
+            logger.LogError("test");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ src/console-log/Readme.md
@@ -0,0 +1,19 @@
+# DI Logging
+
+## Create Template form console
+```
+dotnet new console 
+```
+
+## add Microsoft.Extensions.DependencyInjection Lib
+```
+dotnet add package Microsoft.Extensions.DependencyInjection
+```
+
+## add Microsoft.Extensions.Logging Lib
+
+```
+dotnet add package Microsoft.Extensions.Logging
+dotnet add package Microsoft.Extensions.Logging.Console
+```
+
